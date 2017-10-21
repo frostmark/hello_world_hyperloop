bda4955701d9175eff544133a96a07dd12afad83
Mark Frost <cheerful.mf@gmail.com>
2017-10-21 14:03:21 +0500

    added input to hello world component

--- app/hyperloop/components/helloworld.rb
+++ app/hyperloop/components/helloworld.rb
@@ -30,11 +30,39 @@
       # cleanup any thing (i.e. timers) before component is destroyed
     end
 
+    def show_button
+      BUTTON(class: 'btn btn-info') do
+        'Toggle button'
+      end
+    end
+
+    def show_input
+      H4 do
+        SPAN{ 'Please type ' }
+        SPAN(class: 'colored') { 'Hello World' }
+        SPAN{ ' in the input field below: ' }
+        BR{}
+        SPAN{ 'Or anything you want ' }
+      end
+
+      INPUT(type: :text, class: 'form-control')
+    end
+
+    def show_text
+      H1{ 'input field value will be displayed here' }
+    end
+
     render(DIV) do
       DIV(class: 'hyperloophelloword') do
         IMG(src: 'https://rawgit.com/ruby-hyperloop/hyperloop-js-helloworld/master/hyperloop-logo-medium-white.png?raw=true')
         H3 { "The complete isomorphic ruby framework" }
         BR{}
+
+        show_button
+        DIV(class: 'formdiv') do
+          show_input
+          show_text
+        end
       end
     end
   end
